50446a383ec74d191ae7dc4e7ba8124c9e22a2a0
fluegelcat <2292009362@qq.com>
2023-06-04 14:22:54 +0800

    Create data generator

---
+++ myworld/bank_management_system/data_generator/gen.py
@@ -0,0 +1,22 @@
+import faker
+
+class Gen:
+    def __init__(self):
+        self.fake = faker.Faker(locale='zh_CN')
+
+    def staff(self, num):
+        data = ""
+        for i in range(num):
+            id = self.fake.unique.ssn()
+            name = self.fake.name()
+            phone = self.fake.phone_number()
+            address = self.fake.address()
+            mail = self.fake.email()
+            data += f"{id},{name},{phone},{address},{mail}\n"
+        return data
+        
+if __name__ == "__main__":
+    g = Gen()
+    data = g.staff(5)
+    with open("staff.csv", "w") as f:
+        f.write(data)
